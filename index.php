a33230325449c2b2d7e0a62892d6d71b99f7736a
Rain Liu <laoliuxt@hotmail.com>
2014-09-08 12:56:11 -0400

    asdad
    adssad

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-$mysqli = new mysqli("localhost", "root", "3389200", "test", 3306);
+$mysqli = new mysqli("localhost", "root", "passwd", "test", 3306);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else {
